fc62607af7d5554904242aaaf70dc6654a45118c
Junior197235 <joelitojr9@gmail.com>
2024-06-05 01:45:37 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Joel_Term_Project"
+include(":app")
+ 
